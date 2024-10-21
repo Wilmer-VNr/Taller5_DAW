b13ec68915500d62caa028a42586cb77c53417e6
Wilmer-VNr <150551624+Wilmer-VNr@users.noreply.github.com>
2024-10-20 19:16:41 -0500

    Laboratorio5 - MercadoLibre.js

--- MercadoLibre.js
+++ MercadoLibre.js
@@ -23,3 +23,34 @@ const getMercadoLibreData = async () => {
 };
 
 getMercadoLibreData();
+
+
+// PROMESAS
+// FETCH - THEN - TRY Y CATCH
+
+const url = "https://api.mercadolibre.com/sites/MLA/search?q=celulares"
+
+
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Datos obtenidos:");
+        console.log(data);
+    })
+    .catch((error) => console.log("Error:", error));
+
+
+// AWAIT Y ASYCN 
+
+const getMercadoLibreDatos = async () => {
+    try {
+        let response = await fetch("https://api.mercadolibre.com/sites/MLA/search?q=celulares")
+        let data = await response.json();
+        console.log("Datos obtenidos con Async/Await:");
+        console.log(data);
+    } catch (error) {
+        console.log("Error con Async/Await:", error);
+    }
+};
+
+getMercadoLibreDatos();
